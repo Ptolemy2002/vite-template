db7a8d80c02195ebb582d5a89f093f40a7cd9922
Ptolemy Henson <ptolemy2002@gmail.com>
2024-10-05 12:57:32 -0400

    Added app-ts template

--- app-ts/vite.config.ts
+++ app-ts/vite.config.ts
@@ -1,10 +1,8 @@
 import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(".");
 
 // https://vitejs.dev/config/
 export default defineConfig({
